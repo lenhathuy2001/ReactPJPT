60952884d904adfa6b33c0dd21b960859ba76044
lenhathuy2001 <lenhathuy5060@gmail.com>
2023-03-16 11:46:37 +0700

    react portal

---
+++ personal-portfolio/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import { Banner } from './components/Banner';
+import { NavBar } from './components/NavBar';
+import { Skills } from './components/Skills';
+import { Projects } from './components/Projects';
+import { Footer } from './components/Footer';
+import'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div classNme="App">
+      <NavBar />
+      <Banner />
+      <Skills />
+      <Projects />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ personal-portfolio/src/components/Banner.js
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react"
+import { Col, Container, Row } from "react-bootstrap"
+import { ArrowRightCircle } from "react-bootstrap-icons"
+import headerImg from "../assets/img/header-img.svg"
+import 'animate.css';
+import TrackVisibility from "react-on-screen";
+
+export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const toRolate = ["Purple You", "Mikrokosmos", "Your Universe"];
+  const [text, setText] = useState('');
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const period = 2000;
+
+  useEffect(() => {
+    let ticker = setInterval(() =>{
+        tick();
+    }, delta)
+
+    return () => { clearInterval(ticker) };
+  },[text])
+
+  const tick = () => {
+    let i = loopNum % toRolate.length;
+    let fulltext = toRolate[i];
+    let updatedText = isDeleting ? fulltext.substring(0, text.length - 1) : fulltext.substring(0, text.length + 1);
+
+    setText(updatedText);
+    
+    if (isDeleting) {
+      setDelta(prevDelta => prevDelta /2)
+    }
+
+    if(!isDeleting && updatedText === fulltext) {
+      setIsDeleting(true);
+      setDelta(period);
+    } else if(isDeleting && updatedText === ''){
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setDelta(500);
+    }
+  }
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="align-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <TrackVisibility>
+            {({isVisible}) =>
+            <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+              <span className="tagline">Welcome to my Universe</span>
+              <h1>{`Hi I'm Jin`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Purple You", "Mikrokosmos", "Your Universe" ]'><span className="wrap">{text}</span></span></h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis, orci eu auctor dictum, orci diam molestie mi, rhoncus dignissim nisi lorem nec arcu. Nulla pharetra ligula nec dignissim tempus.</p>
+              <button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size={25}/></button>
+            </div>}
+            </TrackVisibility>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+              <img src={headerImg} alt="Header img" />
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  )
+}
